fe92ba926cabbe89da9517268e4a94e7112c9851
srvacharya <108749250+srvacharya@users.noreply.github.com>
2023-12-23 20:17:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
-const express= require("express");
-const path= require ("path");
-const app=express();
-const mongoose= require("mongoose");
-const User=require('./model.js');
-const mydata="mongodb+srv://3226saurav:Lilwayne2222@cluster0.yqhnun0.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(mydata,{useNewUrlParser:true,useUnifiedTopology:true});
-mongoose.connection.on('connected',()=>{
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser"); // Add this line
+const app = express();
+const mongoose = require("mongoose");
+const User = require('/home/saurav/Documents/node auth/model.js');
+
+const mydata = "mongodb+srv://3226saurav:Eminem2222@cluster0.oif7qqw.mongodb.net/nod_auth?retryWrites=true&w=majority";
+const db=mongoose.connect(mydata, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('connected', () => {
     console.log("connection established to mongodb");
-})
-app.use(express.json());
-app.use(express.urlencoded());
+});
+  app.use(express.json());
 
-const port=3700;
+// Replace express.urlencoded() with bodyParser.urlencoded()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error('Error getting users:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-app.get('/',(req, res)=> {
-    res.sendFile(path.join(__dirname,'form.html'));
-})
 
-app.post("/signup",async(req,res)=>{
-    const{email,password}=req.body;
-    const newUser=new User({email,password});
-    newUser.save();
-   
-})
-app.listen(port,()=>{
-    console.log("The server is listening ")
-})
+
+const port = 3800;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'form.html'));
+});
+
+
+
+app.post("/signup", async(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(email, password);
+
+    try {
+        
+        const existingUser= await User.findOne({email:email})
+        const existingPassword=await User.findOne({password:password})
+    if(existingUser && existingPassword){
+        res.sendFile(path.join(__dirname,'socket.html'))
+    }
+    else{
+        res.send("please try again")
+    }
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.listen(port, () => {
+    console.log("The server is listening ");
+});
